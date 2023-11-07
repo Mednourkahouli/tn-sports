eb1b039657a37a8f11589e0b9b239360c7383a41
Mednourkahouli <nourkahouli13@gmail.com>
2023-11-07 15:11:06 +0100

    tn-sports

---
+++ about.php
@@ -0,0 +1,85 @@
+  <!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <style>
+  body {
+    margin: 0;
+    font-family: Arial, Helvetica, sans-serif;
+      background-position: center;
+  background-image: url("imagens/bg.png");
+  background-color: white;
+  background-size: cover;
+  background-attachment: fixed;
+  background-repeat: no-repeat;
+  }
+
+  .topnav {
+    overflow: hidden;
+    background-color: #333;
+  }
+
+  .topnav a {
+    float: left;
+    color: #f2f2f2;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+    font-size: 17px;
+  }
+
+  .topnav a:hover {
+    background-color: #ddd;
+    color: black;
+  }
+
+  .topnav a.active {
+    background-color: #4CAF50;
+    color: white;
+  }
+
+  .center{
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    width: 50%;
+  }
+  div {
+    text-align: center;
+  }
+  </style>
+  <link rel="shortcut icon" type="image/x-icon" href="imagens/logo.png" />
+    <title>Tn-Masters</title>
+  </head>
+  <body>
+
+  <div class="topnav">
+    <a href="principal.html" target = "_self" >Home</a>
+    <a href="teams.html" target = "_self" >Team</a>
+    <a href="news.html" target = "_self" >News</a>
+    <a class="active" href="about.html" target = "_self" >About</a>
+  </div>
+
+  <div style="padding-left:16px">
+
+  <a><img src="C:\xampp\htdocs\Tn-Masters\win.jpg" width="100%" height="800px"></a>
+  
+ Tn-Masters player holding a trophy after winning a championship.<br>
+Tn-Masters is a world-renowned professional gaming organization established in 2000.
+<br> Since the beginning of our activities, we have dominated the general gaming scene and with that
+we have become a complete team, fully respected in the E-Sports area.
+
+<br> With more than 60 championships won and the best players in the world, Tn-Masters
+is synonymous with success. Our teams won the biggest prize in the history of E-sports
+and are collecting trophies and titles around the world.
+
+Today, Tn-Masters remains a leader in all championships with innovation and intelligence, one of the biggest
+of Brazil currently. 
+
+  
+
+  </div>
+
+  </body>
+  </html>
---
+++ news.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+    background-position: center;
+  background-image: url("imagens/bg.png");
+  background-color: white;
+  background-size: cover;
+  background-attachment: fixed;
+  background-repeat: no-repeat;
+}
+
+.topnav {
+  overflow: hidden;
+  background-color: #333;
+}
+
+.topnav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.topnav a.active {
+  background-color: #4CAF50;
+  color: white;
+}
+</style>
+<link rel="shortcut icon" type="image/x-icon" href="imagens/logo.png" />
+  <title>Tn-Masters</title>
+</head>
+<body>
+
+<div class="topnav">
+  <a href="principal.html" target = "_self" >Home</a>
+  <a href="teams.html" target = "_self" >Team</a>
+  <a class="active" href="news.html" target = "_self" >Feedback</a>
+  <a href="about.html" target = "_self" >About</a>
+</div>
+
+<p>
+  <div style="padding-left:16px">
+  
+<form action="obrigado.html" method="post">
+  Name:
+  <input name="firstname" type="text">
+  <br><br>
+  Gender:
+  <select>
+    <option value = "masculino"> Masculine</option>
+    <option value = "feminino"> Feminine</option>
+    <option value = "outro"> Other</option>
+  
+</select>
+  <br><br>
+  Age Range: <br><br>
+  <input type="radio" name="idade" value = "18"> Under 18<br>
+  <input type="radio" name="idade" value = "entre"> Between 18 and 25<br>
+  <input type="radio" name="idade" value = "acima"> Over 25<br>
+
+  <br>What would you like to see on our website? <br><br>
+
+  <textarea name="message" rows="10" cols="35">
+  </textarea><br><br>
+
+  <input type="reset" value = "To go back">
+  <input type="submit" value = "Confirm">
+  
+</form>
+
+</div>
+
+
+</body>
+</html>
